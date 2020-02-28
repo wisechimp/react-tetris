f4dc446afd0bbdcdc65f65138ab9e250ae14ee6c
shicks1080 <stuart.hicks@gmail.com>
2020-02-28 14:46:07 +0100

    Adding the code to control the score, row count and level
    Made a right pigs ear of bringing this into the Tetris component. Check out the scrum and be very embarassed to be reminded of this!

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -6,6 +6,7 @@ import { createStage, checkCollision } from '../gameUtils/gameHelpers'
 import { useInterval } from '../hooks/useInterval' // https://overreacted.io/making-setinterval-declarative-with-react-hooks/
 import { usePlayer } from '../hooks/usePlayer'
 import { useStage } from '../hooks/useStage'
+import { useGameStatus } from '../hooks/useGameStatus'
 
 // Components
 import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris'
@@ -18,7 +19,10 @@ export default () => {
   const [gameOver, setGameOver] = useState(false)
 
   const [player, updatePlayerPosition, resetPlayer, rotatePlayer] = usePlayer();
-  const [stage, setStage] = useStage(player, resetPlayer)
+  const [stage, setStage, rowsCleared] = useStage(player, resetPlayer)
+  const [score, setScore, rows, setRows, level, setLevel] = useGameStatus(
+    rowsCleared
+  );
 
   console.log('Re-render')
 
@@ -34,12 +38,23 @@ export default () => {
     setDropTime(1000)
     resetPlayer()
     setGameOver(false)
+    setScore(0)
+    setRows(0)
+    setLevel(0)
   }
 
   const drop = () => {
+    // Increase level when player has cleared 10 rows
+    if (rows > (level + 1) * 10) {
+      setLevel(previous => previous + 1)
+      //Also increase the speed
+      setDropTime(1000 / (level + 1) + 200);
+    }
+    
     if (!checkCollision(player, stage, { x: 0, y: 1 })){
       updatePlayerPosition({ x: 0, y: 1, collided: false })
     } else {
+      // Game over
       if (player.position.y < 1) {
         console.log('Game Over !!!')
         setGameOver(true)
@@ -53,7 +68,7 @@ export default () => {
     if (!gameOver) {
       if (keyCode === 40) {
         console.log('Interval on')
-        setDropTime(1000)
+        setDropTime(1000 / (level + 1));
       }
     }
   }
@@ -89,18 +104,23 @@ export default () => {
   }, dropTime)
 
   return (
-    <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)} onKeyUp={keyUp}>
+    <StyledTetrisWrapper
+      role="button"
+      tabIndex="0"
+      onKeyDown={e => move(e)}
+      onKeyUp={keyUp}
+    >
       <StyledTetris>
         <Stage stage={stage} />
         <aside>
           {gameOver ? (
             <Display gameOver={gameOver} text="Game Over" />
           ) : (
-          <div>
-            <Display text="Score" />
-            <Display text="Rows" />
-            <Display text="Level" />
-          </div>
+            <div>
+              <Display text={`Score: ${score}`} />
+              <Display text={`Rows: ${rows}`} />
+              <Display text={`Level: ${level}`} />
+            </div>
           )}
           <StartButt callback={startGame} />
         </aside>
